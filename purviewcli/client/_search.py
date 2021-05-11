a9abe54618047a378482fd5d3b14b95857d5f28d
Taygan Rifat <contact@taygan.co>
2021-05-11 23:26:41 +0100

    search query

--- purviewcli/client/_search.py
+++ purviewcli/client/_search.py
@@ -1,17 +1,21 @@
 from .client import get_data
+import json
 
 # ---------------------------
 # SEARCH
 # ---------------------------
 def searchQuery(args):
     endpoint = '/api/search/query'
+
     payload = {
         'keywords': args['--keywords'],
-        'filter': args['--filter'],
         'limit': args['--limit'],
-        'offset': args['--offset'],
-        'facets': args['--facets']
+        'offset': args['--offset']
     }
+    if '--filter' in args:
+        payload['filter'] = json.loads(args['--filter'])
+    if '--facets' in args:
+        payload['facets'] = json.loads(args['--facets'])
 
     http_dict = {'app': 'catalog', 'method': 'POST', 'endpoint': endpoint, 'params': None, 'payload': payload}
     data = get_data(http_dict)
